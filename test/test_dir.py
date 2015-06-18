7754293e9cf9989386a9f1f1134daeebe8836228
Xiang Li <xiang@whzmouse.(none)>
2015-06-18 21:22:53 +0800

    path test mini program.

---
+++ test/test_dir.py
@@ -0,0 +1,9 @@
+#coding: utf-8
+
+import sys,os,time
+reload(sys).setdefaultencoding('utf-8')
+
+if __name__ == "__main__":
+	print 'hello.'
+	print 'current working directory:', os.getcwd()
+	print sys.path
